e6373e0d29e8af301041ffc872f59a611dbd2edb
Wayan Tisna <wayantisna74@gmail.com>
2022-08-03 16:32:23 +0800

    update

--- _nuxt/953f689.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{859:function(e,n,t){"use strict";t.r(n);t(36);n.default={checkRoleContains:function(e){for(var n=this.$auth.user.roles,t="",i=0;i<this.$auth.user.roles.length;i++)t=n[i].name===e;return t}}}}]);
